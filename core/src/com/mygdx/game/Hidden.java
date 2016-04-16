6cd94eaf8f91ca902a6308303f6d27c50168524c
Ethan Raymond <ethan.raymond14@gmail.com>
2016-04-16 18:26:19 +0800

    basic track pad and blob

--- core/src/com/mygdx/game/Hidden.java
+++ core/src/com/mygdx/game/Hidden.java
@@ -2,26 +2,125 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 
 public class Hidden extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	ShapeRenderer shapeRenderer;
+	Touchpad touchpad;
+    private OrthographicCamera camera;
+    private Stage stage;
+    private Touchpad.TouchpadStyle touchpadStyle;
+    private Skin touchpadSkin;
+    private Drawable touchBackground;
+    private Drawable touchKnob;
+    private Texture blockTexture;
+    private Sprite blockSprite;
+    private float blockSpeed;
 	
 	@Override
 	public void create () {
+		shapeRenderer = new ShapeRenderer();
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        //Create camera
+        float aspectRatio = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 10f*aspectRatio, 10f);
+
+        //Create a touchpad skin
+        touchpadSkin = new Skin();
+        //Set background image
+        //FileHandle files = Gdx.files.internal("touchBackground.png");
+        //System.out.println(files.toString());
+        img = new Texture("badlogic.jpg");
+        touchpadSkin.add("touchBackground", new Texture("touchBackground.png"));
+        //Set knob image
+        touchpadSkin.add("touchKnob", new Texture("touchKnob.png"));
+        //Create TouchPad Style
+        touchpadStyle = new Touchpad.TouchpadStyle();
+        //Create Drawable's from TouchPad skin
+        touchBackground = (Drawable) touchpadSkin.getDrawable("touchBackground");
+        touchKnob = (Drawable) touchpadSkin.getDrawable("touchKnob");
+        //Apply the Drawables to the TouchPad Style
+        touchpadStyle.background = (com.badlogic.gdx.scenes.scene2d.utils.Drawable) touchBackground;
+        touchpadStyle.knob = (com.badlogic.gdx.scenes.scene2d.utils.Drawable) touchKnob;
+        //Create new TouchPad with the created style
+        touchpad = new Touchpad(10, touchpadStyle);
+        //setBounds(x,y,width,height)
+        touchpad.setBounds(15, 15, 200, 200);
+
+        //Create a Stage and add TouchPad
+        //stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true, batch);
+        stage = new Stage();
+        //Use batch????????
+        stage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+        stage.addActor(touchpad);
+        Gdx.input.setInputProcessor(stage);
+
+        //Create block sprite
+        blockTexture = new Texture(Gdx.files.internal("block.png"));
+        blockSprite = new Sprite(blockTexture);
+        //Set position to centre of the screen
+        blockSprite.setPosition(Gdx.graphics.getWidth()/2-blockSprite.getWidth()/2, Gdx.graphics.getHeight()/2-blockSprite.getHeight()/2);
+
+        blockSpeed = 5;
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+    @Override
+    public void render() {
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.circle(50, 50, 32);
+        shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.end();
+
+
+        Gdx.gl.glClearColor(0.294f, 0.294f, 0.294f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+
+        //Move blockSprite with TouchPad
+        blockSprite.setX(blockSprite.getX() + touchpad.getKnobPercentX()*blockSpeed);
+        blockSprite.setY(blockSprite.getY() + touchpad.getKnobPercentY()*blockSpeed);
+
+        //Draw
+        batch.begin();
+        blockSprite.draw(batch);
+        batch.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 	}
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
 }
